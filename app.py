d9296fb86a7b32629cfd82b2370c8fee2d5a536d
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-03-20 10:34:30 -0400

    feature qui marche

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from mysql.connector import connect
 from json import loads
 import requests
 
 URL_API_PHOTO_CHIEN = "https://dog.ceo/api/breeds/image/random"
 application = Flask('glo2005')
+
+MYSQL_URI = "localhost"
+PORT = "1337"
+USERNAME = "root"
+PASSWORD = None
+DATABASE_NAME = "what_is_it"
+
+database_connector = connect(host=MYSQL_URI, port=PORT, user=USERNAME, password=PASSWORD, database=DATABASE_NAME)
 
 
 @application.route('/')
@@ -18,5 +27,33 @@ def photo_de_chien():
 
     return render_template('image_animal.html', url =json_de_lapi["message"], animal = "chien" )
 
+@application.route("/vote", methods=["POST"])
+def vote():
+    add_vote = ("INSERT INTO vote "
+                    "(image_url, value)"
+                    "VALUES (%s, %s)")
+
+    vote_values = (request.values['image_url'], request.values['vote_value'])
+    cursor = database_connector.cursor()
+    cursor.execute(add_vote, vote_values)
+    database_connector.commit()
+
+    return photo_de_chien()
+
+@application.route("/vote/<animal>", methods=["GET"])
+def get_vote(animal):
+    select_animal = ("SELECT image_url FROM vote "
+                    "WHERE value = %s")
+
+    vote_values = (animal,)
+    cursor = database_connector.cursor()
+    cursor.execute(select_animal, vote_values)
+    output = ""
+    for vote in cursor:
+        output += "<img src={}><br>".format(vote[0])
+
+    return output
+
+    return photo_de_chien()
 
 application.run('0.0.0.0',8000)
